bd8c9b6535e7f71130c96e98060657a3f67dc05b
matsuoka18 <82459435+matsuoka18@users.noreply.github.com>
2023-02-06 18:57:58 +0900

    Add files via upload

--- js-商品表示2.js
+++ js-商品表示2.js
@@ -245,7 +245,7 @@ sn='';
 sn2 ='';
 cd="";
 function start(){
- /* cd= document.cookie;
+ cd= document.cookie;
  // cd = 'branch=menu|; menumemo={"shop1":[0,22,38],"shop7":[14,36,52],"shop13":[4,26,42],"shop19":[1,23,39],"shop25":[1,23,39],"shop31":[1,23,39],"shop37":[1,23,39],"shop43":[1,23,39],"shop49":[1,23,39],"shop55":[23,39],"shop61":[23,39],"shop67":[23,39],"shop73":[23,39],"shop79":[23,39],"last":[79]}-; loginID=%E5%85%AB%E7%99%BE%E5%B1%8B!; pass=%E3%82%84%E3%81%BE%E3%81%8D?; shopnumber=85#; searchname=hello^;searchnum=85$';
 console.log(cd);
       num1 = cd.indexOf("searchnum=");
@@ -282,9 +282,7 @@ console.log(cd);
    document.getElementById("shopname").innerHTML = sn2;
    document.getElementById("shopname2").innerHTML = sn2;  
       sn = cd.substring(num1+10,num2);
-      sn2 = decodeURI(cd.substring(num3+11,num4));*/
-            sn = 1;
-      sn2 ="hhh";
+      sn2 = decodeURI(cd.substring(num3+11,num4));
       console.log(sn+":"+sn2);
      setTimeout(startt,1000)
      }
