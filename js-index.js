9c6f4be2137632a7c697aa9d22b6ee8277b6b4d9
matsuoka18 <82459435+matsuoka18@users.noreply.github.com>
2023-02-06 21:53:57 +0900

    Add files via upload

--- js-index.js
+++ js-index.js
@@ -31,13 +31,184 @@ function pic(){
   if(a == 0){
     a = 1;
   document.getElementById("img").src="food.jpeg";
+  document.getElementById("img2").src="IMG_0064.JPG";
+  $("#img").animate({
+    "width":'90vw',
+    "height":'51vw',
+    "left":0,
+    "top":0
+  },{
+    'duration':0
+  })
+  $("#im2").animate({
+    "opacity":1
+  },{
+    'duration':0
+  })
+  $("#img2").animate({
+    'left':'90vw',
+    'top':'2.5vw'
+   },{
+     'duration':0
+   })
+  $("#im1").animate({
+    "opacity":0.5
+  },{
+    'duration':0
+  })
+
+  $("#img").animate({
+    "width":'90vw',
+    "height":'51vw',
+    "left":0,
+    "top":0
+  },{
+    'duration':4000
+  })
+  $("#im2").animate({
+    "opacity":1
+  },{
+    'duration':4000
+  })
+  $("#img2").animate({
+    'left':0,
+    'top':0
+   },{
+     'duration':1000
+   })
+  $("#im1").animate({
+    "opacity":0.5
+  },{
+    'duration':1000
+  })
+
+  $("#img2").animate({
+    'left':0,
+    'top':0
+   },{
+     'duration':3000
+   })
+  $("#im1").animate({
+    "opacity":0.5
+  },{
+    'duration':3000
+  })
+
+  $("#img").animate({
+    "left":'90vw',
+    "top":0
+  },{
+    'duration':1000
+  })
+  $("#im2").animate({
+    "opacity":0.5
+  },{
+    'duration':1000
+  })
+  $("#img2").animate({
+    'left':'2.5vw',
+    'top':'2.5vw'
+   },{
+     'duration':1000
+   })
+  $("#im1").animate({
+    "opacity":1
+  },{
+    'duration':1000
+  })
   setTimeout(pic,5000);
   }else{
     a = 0;
     document.getElementById("img").src="IMG_0064.JPG";
+    document.getElementById("img2").src="food.jpeg";
+    $("#img").animate({
+      "width":'90vw',
+      "height":'51vw',
+      "left":0,
+      "top":0
+    },{
+      'duration':0
+    })
+    $("#im2").animate({
+      "opacity":1
+    },{
+      'duration':0
+    })
+    $("#img2").animate({
+      'left':'90vw',
+      'top':'2.5vw'
+     },{
+       'duration':0
+     })
+    $("#im1").animate({
+      "opacity":0.5
+    },{
+      'duration':0
+    })
+
+    $("#img").animate({
+      "width":'90vw',
+      "height":'51vw',
+      "left":0,
+      "top":0
+    },{
+      'duration':4000
+    })
+    $("#im2").animate({
+      "opacity":1
+    },{
+      'duration':4000
+    })
+    $("#img2").animate({
+      'left':0,
+      'top':0
+     },{
+       'duration':1000
+     })
+    $("#im1").animate({
+      "opacity":0.5
+    },{
+      'duration':1000
+    })
+
+    $("#img2").animate({
+      'left':0,
+      'top':0
+     },{
+       'duration':3000
+     })
+    $("#im1").animate({
+      "opacity":0.5
+    },{
+      'duration':3000
+    })
+
+    $("#img").animate({
+      "left":'90vw',
+      "top":0
+    },{
+      'duration':1000
+    })
+    $("#im2").animate({
+      "opacity":0.5
+    },{
+      'duration':1000
+    })
+    $("#img2").animate({
+      'left':'2.5vw',
+      'top':'2.5vw'
+     },{
+       'duration':1000
+     })
+    $("#im1").animate({
+      "opacity":1
+    },{
+      'duration':1000
+    })
     setTimeout(pic,5000);
+  }
 }
-}
+
 function shop(){
 location.href="登録ページ.html";
 }
