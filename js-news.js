af477f050559708ad72e25c77e7aa25e91dcdcaa
matsuoka18 <82459435+matsuoka18@users.noreply.github.com>
2023-02-10 19:44:33 +0900

    Add files via upload

--- js-news.js
+++ js-news.js
@@ -1,3 +1,7 @@
 function c1(){
-    window.parent.c11();
+    //document.getElementById("all1").style.display = "none";
+    document.getElementById('c11').style.backgroundColor = "lightgray";
+}
+function c1n(){
+    document.getElementById('c11').style.opacity = "1";
 }
