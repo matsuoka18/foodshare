9160acacb1877f86c737c6b0cb59a185776827ec
matsuoka18 <82459435+matsuoka18@users.noreply.github.com>
2023-04-06 16:27:18 +0900

    Add files via upload

---
+++ test-ajax/test.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+import cgi
+
+storage = cgi.FieldStorage()
+print('Status: 200 OK')
+print('Content-Type: text/html\n')
+data = storage.getvalue('data')
+recieve = data + ":成功"
+print(recieve)
