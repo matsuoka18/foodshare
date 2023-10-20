4ad1224da3e8b3e73e68d1f8841584c8eee4d658
matsuoka18 <82459435+matsuoka18@users.noreply.github.com>
2023-10-21 00:38:57 +0900

    Add files via upload

--- js-menus.js
+++ js-menus.js
@@ -1,8 +1,8 @@
         url = "https://script.google.com/macros/s/AKfycbwBH_VrPaXcJg8HOXfoWHJY8f0Ir3935fqlJlURpyAkd8IdEQ/exec";
 function start(){
     document.getElementById("bt1").innerHTML = "Loading";
-  //cd= document.cookie;
-    cd = 'branch=menu|; menumemo={"shop1":[22,38],"shop7":[14,36,52],"shop13":[4,26,42],"shop19":[1,23,39],"shop25":[1,23,39],"shop31":[1,23,39],"shop37":[1,23,39],"shop43":[1,23,39],"shop49":[1,23,39],"shop55":[23,39],"shop61":[23,39],"shop67":[23,39],"shop73":[23,39],"shop79":[23,39],"last":[79]}-; loginID=%E5%85%AB%E7%99%BE%E5%B1%8B!; pass=%E3%82%84%E3%81%BE%E3%81%8D?; shopnumber=85#';
+  cd= document.cookie;
+    //cd = 'branch=menu|; menumemo={"shop1":[22,38],"shop7":[14,36,52],"shop13":[4,26,42],"shop19":[1,23,39],"shop25":[1,23,39],"shop31":[1,23,39],"shop37":[1,23,39],"shop43":[1,23,39],"shop49":[1,23,39],"shop55":[23,39],"shop61":[23,39],"shop67":[23,39],"shop73":[23,39],"shop79":[23,39],"last":[79]}-; loginID=%E5%85%AB%E7%99%BE%E5%B1%8B!; pass=%E3%82%84%E3%81%BE%E3%81%8D?; shopnumber=85#';
 console.log(cd);
         num1 = cd.indexOf("searchnum=");
         console.log(num1);
