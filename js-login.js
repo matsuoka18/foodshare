fd7a57a8c83a69384f13a6d6b276a9e1961fd049
matsuoka18 <82459435+matsuoka18@users.noreply.github.com>
2023-10-20 21:14:16 +0900

    Add files via upload

--- js-login.js
+++ js-login.js
@@ -1,11 +1,13 @@
 function te(){
-    document.getElementById("but3").style.visibility = "none";
+    document.getElementById("but3").style.display = "none";
    document.getElementById("but3").style.opacity = "0";
    }
+   
       function login(){
+        console.log("login START");
         document.getElementById("but4").style.visibility = "none";
         document.getElementById("but4").style.opacity = "0";
-        document.getElementById("but3").style.visibility = "visible";
+        document.getElementById("but3").style.display = "block";
         document.getElementById("but3").innerHTML = "Sending...";
         document.getElementById("but3").style.opacity = "1";
         var data=[{
@@ -18,9 +20,11 @@ function te(){
           "body":JSON.stringify(data)
         }
         fetch('https://script.google.com/macros/s/AKfycbwBH_VrPaXcJg8HOXfoWHJY8f0Ir3935fqlJlURpyAkd8IdEQ/exec',params)
-      setTimeout(login12,3000);
+      console.log("login FIN");
+        setTimeout(login12,3000);
       }
       function login12(){
+        console.log("login2 START")
         document.getElementById("but3").innerHTML = "loading...";
         fetch('https://script.google.com/macros/s/AKfycbwBH_VrPaXcJg8HOXfoWHJY8f0Ir3935fqlJlURpyAkd8IdEQ/exec', {
             method: "GET",
@@ -30,7 +34,8 @@ function te(){
             if (response.ok) {
               document.getElementById("but3").style.visibility = "hidden";
     document.getElementById("but3").style.opacity = "0";
-     document.getElementById("but3").innerHTML = "";
+    document.getElementById("but3").style.display = "none";
+     document.getElementById("but3").innerHTML = "sending...";
     document.getElementById("but4").style.visibility = "visible";
     document.getElementById("but4").style.opacity = "1";
               return response.json();
@@ -51,14 +56,15 @@ function te(){
             c = 0;
             for(var i = 0; i<sum; i++){
               text2 = text.logindata[0];
-              name ="店舗"+i;
-              shop = text2[name];
+              namee ="店舗"+i;
+              shop = text2[namee];
               console.log(shop)
               if(shop == inputtext){
-                number = name;
+                number = namee;
                 number2 =i;
                 i = sum+1;
                 c = 1;
+                console.log("login12 FIN")
                 login2();
               }
               }
@@ -70,7 +76,7 @@ function te(){
           .catch(error => {
             // ネットワークエラーの場合はここに到達する
             console.error(error);
-                                document.getElementById("but3").style.visibility = "hidden";
+    document.getElementById("but3").style.visibility = "hidden";
     document.getElementById("but3").style.opacity = "0";
      document.getElementById("but3").innerHTML = "";
     document.getElementById("but4").style.visibility = "visible";
@@ -79,6 +85,7 @@ function te(){
           })
       }
 function login2(){
+  console.log("login2 START");
 name2 = "パスワード"+number2;
 name3 = "取得用"+number2;
 password = text2[name2];
@@ -95,6 +102,7 @@ if(password == inputpassword){
   document.cookie = loginID;
   document.cookie = pass;
   document.cookie = shopnumber;
+  console.log("login2 FIN")
   location.href="shoped.html";
 }else{
   alert("パスワードが間違っています")
