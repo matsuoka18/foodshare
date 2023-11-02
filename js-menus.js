c5aa9cc51d648431723f79cbb776399f110881f6
matsuoka18 <82459435+matsuoka18@users.noreply.github.com>
2023-11-03 00:23:47 +0900

    Add files via upload

--- js-menus.js
+++ js-menus.js
@@ -1,9 +1,12 @@
         url = "https://script.google.com/macros/s/AKfycbwBH_VrPaXcJg8HOXfoWHJY8f0Ir3935fqlJlURpyAkd8IdEQ/exec";
+        sn5 = '';
 function start(){
     document.getElementById("bt1").innerHTML = "Loading";
   cd= document.cookie;
     //cd = 'branch=menu|; menumemo={"shop1":[22,38],"shop7":[14,36,52],"shop13":[4,26,42],"shop19":[1,23,39],"shop25":[1,23,39],"shop31":[1,23,39],"shop37":[1,23,39],"shop43":[1,23,39],"shop49":[1,23,39],"shop55":[23,39],"shop61":[23,39],"shop67":[23,39],"shop73":[23,39],"shop79":[23,39],"last":[79]}-; loginID=%E5%85%AB%E7%99%BE%E5%B1%8B!; pass=%E3%82%84%E3%81%BE%E3%81%8D?; shopnumber=85#';
     //cd ='branch=menu|; menumemo={"shop1":["3","21","24","37","40","107","108","109"],"shop7":["3","9","19","22","41","44","57","60"],"shop13":["9","12","31","34","47","50"],"shop19":["6","9","28","31","44","47"],"shop25":["6","9","28","31","44","47"],"last":["25"]}-';
+    cd = 'branch=menu|; menumemo={"shop1":["0","15","31"],"shop7":["6","12","16","38","54"],"shop13":["6","28","44"],"shop19":["3","25","41"],"shop25":["3","25","41"],"shop31":["3","25","41"],"last":["31"]}-; cookie_menuname=%E3%81%82%E3%81%84%E3%81%86%E3%81%88%E3%81%8A-; firstdata1={"shopdata":[{"n1":"hhh","n7":"abc","n13":"大船中学校","n19":"ばーーか","n25":"TULLYS COFFEE","n31":"店舗","n37":"手広","n43":"東進","n49":"マツオ","n55":"ローソン","num":55}]}; datav=0'
+
     console.log(cd);
         num1 = cd.indexOf("searchnum=");
         console.log(num1);
@@ -20,8 +23,11 @@ function start(){
             console.log("1");
             sn3 = cd.substring(num5+7,num6);
         sn4 = cd.substring(num7+9,num8);
+        sn5 = cd.substring(num9+16,num10);
+        sn5 = decodeURI(sn5);
         console.log(sn3);
         console.log(sn4);
+        console.log("sn5:"+sn5);
         }else{
         sn = cd.substring(num1+10,num2);
         sn2 = decodeURI(cd.substring(num3+11,num4));
